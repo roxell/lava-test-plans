323cc4651c54160f9354a9e7ac61600972ce073b
Anders Roxell <anders.roxell@linaro.org>
2019-11-25 11:24:50 +0100

    rename DEVICE_TYPE to BOARD_TYPE
    Use BOARD_TYPE since its not the DEVICE_TYPE in LAVA.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- selftests.sh
+++ selftests.sh
@@ -7,19 +7,19 @@ function run_device_selftest()
 }
 
 exit_code=0
-for DEVICE_TYPE in ./devices/*
+for BOARD_TYPE in ./devices/*
 do
     for TEST in ./testcases/*.yaml
     do
-        if [ -d $DEVICE_TYPE ]
+        if [ -d $BOARD_TYPE ]
         then
-            for NESTED_DEVICE_TYPE in $DEVICE_TYPE/*
+            for NESTED_BOARD_TYPE in $BOARD_TYPE/*
             do
-                device_exit_code=$(run_device_selftest $(basename $DEVICE_TYPE)/$(basename $NESTED_DEVICE_TYPE) $(basename $TEST))
+                device_exit_code=$(run_device_selftest $(basename $BOARD_TYPE)/$(basename $NESTED_BOARD_TYPE) $(basename $TEST))
                 if [ $device_exit_code -gt $exit_code ]; then exit_code=$device_exit_code; fi
             done
         else
-            device_exit_code=$(run_device_selftest $(basename $DEVICE_TYPE) $(basename $TEST))
+            device_exit_code=$(run_device_selftest $(basename $BOARD_TYPE) $(basename $TEST))
             if [ $device_exit_code -gt $exit_code ]; then exit_code=$device_exit_code; fi
         fi
     done
